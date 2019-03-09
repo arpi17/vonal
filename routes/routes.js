f10e98d72b7c1c278ab10de7ab403b03acec7551
Arpad Illyes <arpadillyes93@gmail.com>
2019-03-09 15:25:23 +0100

    Add validation to route creating

--- routes/routes.js
+++ routes/routes.js
@@ -5,18 +5,23 @@ const passport = require('passport');
 // Map Model
 const Route = require('../models/Route');
 
+// Validator
+const validateRoute = require('../validation/route');
+
 // @route   GET /routes
 // @desc    Get all (public) routes
-// @query   country=country_name&city=city_name
+// @query   country=country_name&city=city_name&type=type_name
 // @access  Public
 router.get('/', (req, res) => {
+  req.errors = {};
   const routeQuery = {};
-  const queryFields = ['country', 'city'];
+  const queryFields = ['country', 'city', 'type'];
   for (field of queryFields) {
     if (req.query[field]) routeQuery[field] = req.query[field];
   }
 
   Route.find(routeQuery)
+    .sort({ date: -1 })
     .then(routes => {
       if (!routes) {
         req.errors.noroutes = 'There are no routes';
@@ -33,6 +38,7 @@ router.get('/', (req, res) => {
 router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
+  validateRoute,
   (req, res) => {
     const routeData = {};
     const routeFields = [
---
+++ validation/route.js
@@ -0,0 +1,35 @@
+const isEmpty = require('lodash.isempty');
+const validator = require('validator');
+
+module.exports = function(req, res, next) {
+  req.errors = {};
+  let { title, description, tags } = req.body;
+  const ignoreWhitespace = { ignore_whitespace: true };
+
+  title = !isEmpty(title) ? title : '';
+
+  // Title
+  if (!validator.isLength(title, { min: 4, max: 50 })) {
+    req.errors.title = 'Title must be between 4 and 50 characters';
+  }
+  if (validator.isEmpty(title, ignoreWhitespace)) {
+    req.errors.title = 'Title field is required';
+  }
+  // Description (if provided)
+  if (description && !validator.isLength(description, { min: 10, max: 500 })) {
+    req.errors.description = 'Title must be between 10 and 500 characters';
+  }
+  // Tags (if provided)
+  if (
+    !isEmpty(tags) &&
+    !validator.isLength(tags.length.toString(), { max: 10 })
+  ) {
+    req.errors.tags = 'A maximum of 10 tags are allowed';
+  }
+
+  if (!isEmpty(req.errors)) {
+    return res.status(404).json(req.errors);
+  }
+
+  next();
+};
