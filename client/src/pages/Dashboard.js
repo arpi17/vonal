07483b028b24e2c3d9c3919d89aa1fbe2dc745ad
Arpad Illyes <arpadillyes93@gmail.com>
2019-03-13 15:14:26 +0100

    Refactor Landing and Dashboard pages to functional components

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,9 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-
-import { logoutUser } from '../actions/authActions';
 
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
@@ -14,55 +10,29 @@ import {
   DashCard
 } from '../styles/StyledDashboard';
 
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-  }
-
-  handleLogoutClick() {
-    this.props.logoutUser();
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <DashMain>
-          <DashCardContainer>
-            <DashCard as={Link} to="/create">
-              create route
-            </DashCard>
-            <DashCard as={Link} to="/discover">
-              discover
-            </DashCard>
-            <DashCard as={Link} to="/my-routes">
-              my routes
-            </DashCard>
-            <DashCard as={Link} to="/bucketlist">
-              bucketlist
-            </DashCard>
-          </DashCardContainer>
-        </DashMain>
-        <Footer />
-      </React.Fragment>
-    );
-  }
+function Dashboard() {
+  return (
+    <React.Fragment>
+      <Header />
+      <DashMain>
+        <DashCardContainer>
+          <DashCard as={Link} to="/create">
+            create route
+          </DashCard>
+          <DashCard as={Link} to="/discover">
+            discover
+          </DashCard>
+          <DashCard as={Link} to="/my-routes">
+            my routes
+          </DashCard>
+          <DashCard as={Link} to="/bucketlist">
+            bucketlist
+          </DashCard>
+        </DashCardContainer>
+      </DashMain>
+      <Footer />
+    </React.Fragment>
+  );
 }
 
-Dashboard.propTypes = {
-  logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
-};
-
-const mapStateToProps = state => ({
-  auth: state.auth
-});
-const mapDispatchToProps = {
-  logoutUser
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Dashboard);
+export default Dashboard;
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 
 // Styled-components
 import { MainTagline, SubTagline } from '../styles/Title';
@@ -9,46 +7,36 @@ import { Grid, GridContent, GridImage } from '../styles/Grid';
 import { Card } from '../styles/Card';
 import { NavList, NavItem } from '../styles/Nav';
 
-class Landing extends Component {
-  render() {
-    return (
-      <Grid areas={'"image image content"'}>
-        <GridImage img={process.env.PUBLIC_URL + '/landing.jpg'}>
-          <MainTagline>Vonal</MainTagline>
-          <SubTagline>Create and discover your own routes</SubTagline>
-        </GridImage>
-        <GridContent>
-          <Card width="240px" top="150px">
-            <NavList>
-              <NavItem as={Link} to="/register">
-                Sign Up
-              </NavItem>
-              <NavItem as={Link} to="/login">
-                Login
-              </NavItem>
-              <NavItem as={Link} to="/discover">
-                Discover
-              </NavItem>
-              <NavItem as={Link} to="/about">
-                About
-              </NavItem>
-              <NavItem as={Link} to="/contact">
-                Contact
-              </NavItem>
-            </NavList>
-          </Card>
-        </GridContent>
-      </Grid>
-    );
-  }
+function Landing() {
+  return (
+    <Grid areas={'"image image content"'}>
+      <GridImage img={process.env.PUBLIC_URL + '/landing.jpg'}>
+        <MainTagline>Vonal</MainTagline>
+        <SubTagline>Create and discover your own routes</SubTagline>
+      </GridImage>
+      <GridContent>
+        <Card width="240px" top="150px">
+          <NavList>
+            <NavItem as={Link} to="/register">
+              Sign Up
+            </NavItem>
+            <NavItem as={Link} to="/login">
+              Login
+            </NavItem>
+            <NavItem as={Link} to="/discover">
+              Discover
+            </NavItem>
+            <NavItem as={Link} to="/about">
+              About
+            </NavItem>
+            <NavItem as={Link} to="/contact">
+              Contact
+            </NavItem>
+          </NavList>
+        </Card>
+      </GridContent>
+    </Grid>
+  );
 }
 
-Landing.propTypes = {
-  auth: PropTypes.object.isRequired
-};
-
-const mapStateToProps = state => ({
-  auth: state.auth
-});
-
-export default connect(mapStateToProps)(Landing);
+export default Landing;
