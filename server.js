6c7375aa6efd3e04901772ae6d4194923cbdf0f4
Arpad Illyes <arpadillyes93@gmail.com>
2019-03-28 01:20:17 +0100

    Prepare to deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const passport = require('passport');
+const path = require('path');
 
 const app = express();
 
@@ -25,6 +26,14 @@ app.use(passport.initialize());
 // Router-level middelware
 app.use('/users', require('./routes/users'));
 app.use('/routes', require('./routes/routes'));
+
+// Serve static HTML in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 PORT = process.env.PORT || 5000;
 
