5ba739af7f5b8532427d087cb76d08092d2b3c98
Arpad Illyes <arpadillyes93@gmail.com>
2019-03-28 14:52:02 +0100

    Modifiy Redux DevTools in production

--- client/src/store.js
+++ client/src/store.js
@@ -9,7 +9,7 @@ const devTools =
   process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__()
-    : null;
+    : f => f;
 
 const store = createStore(
   rootReducer,
