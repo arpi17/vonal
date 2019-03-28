d7883e864359a1c33d3598e063e17e11bf891d2d
Arpad Illyes <arpadillyes93@gmail.com>
2019-03-28 14:35:28 +0100

    Remove Redux DevTools from production build

--- client/src/store.js
+++ client/src/store.js
@@ -5,12 +5,18 @@ import rootReducer from './reducers/rootReducer';
 const initialState = {};
 const middleware = [thunk];
 
+const devTools =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    : null;
+
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
   )
 );
 
